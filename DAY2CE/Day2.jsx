d63fbd1c3b11917cb3842c3b955386bae84af777
Niranjan <150768508+Niranjan284@users.noreply.github.com>
2024-07-31 16:25:52 +0530

    Add files via upload

---
+++ DAY2CE/Day2.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+function Day2c2(){
+    return (
+        <div style={{color:'darkblue',fontSize:'16px'}}>
+            <h1>Inline Style in JSX Example</h1>
+            <p >This is a paragraph with inline styles applied</p>
+        </div>
+    )
+}
+export default Day2c2;
