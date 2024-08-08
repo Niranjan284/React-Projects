d5b3fca1a6d1b6a97ba53081af205131066e8165
Niranjan <150768508+Niranjan284@users.noreply.github.com>
2024-08-08 22:41:25 +0530

    Create Day1pah.jsx

---
+++ Day1pah.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+
+function Pah() {
+    return(
+        <>
+            <div>
+                <h3>
+                  HELLO
+                </h3>
+            </div>
+        
+        </>
+    )    
+}
+
+export default Pah;
